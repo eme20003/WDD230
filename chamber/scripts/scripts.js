d86fd7dafa0c95f475a434da6135c7e746f860f5
Daniel Emerson <eme20003@byui.edu>
2022-10-15 13:41:06 -0700

    JavaScript Items

--- chamber/scripts/scripts.js
+++ chamber/scripts/scripts.js
@@ -5,3 +5,32 @@ function toggleMenu(){
 
 const x = document.getElementById("hamburgerBtn");
 x.onclick = toggleMenu;
+
+const date = new Date();
+let currentDay = date.getDate();
+let currentMonth = date.getMonth() + 1;
+let currentYear = date.getFullYear();
+
+let currentDayOfWeek = date.getDay();
+
+let currentDate = `${currentMonth}/${currentDay}/${currentYear}`;
+
+const displayDate = document.querySelector('p');
+
+displayDate.textContent = 'Date: ' + currentDate;
+
+let newPara = document.createElement('p');
+let newDisplayNode = document.createTextNode('🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m.');
+
+let uList = document.getElementById('actualDate');
+newPara.appendChild(newDisplayNode);
+
+let getDay = () =>{
+    if(currentDayOfWeek === 1 || currentDayOfWeek === 3){
+        uList.appendChild(newPara);
+    }else{
+        uList;
+    }
+}
+
+getDay();

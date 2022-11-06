6405f5eca1880d9d04666a70c2562d8085a8691a
Daniel Emerson <daniel.emerson@gocrossbridge.com>
2022-11-05 18:52:11 -0700

    Form Page

---
+++ chamber/scripts/form.js
@@ -0,0 +1,11 @@
+const regexExpression = new RegExp("[A-Za-z\- ]");
+const firstName = document.querySelector("#firstName").value;
+const lastName = document.querySelector("#lastName").value;
+const businessPosition = document.querySelector("#businessPosition").value;
+const email = document.querySelector("#userEmail").value;
+const phone = document.querySelector("#userPhone").value;
+const businessName = document.querySelector("#businessName").value;
+
+
+
+

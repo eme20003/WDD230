a76058cdc7d148542e521e4cb81f78ac2eb3b2cf
Daniel Emerson <daniel.emerson@gocrossbridge.com>
2022-12-15 21:55:01 -0800

    Final Project Update

--- FinalProject/FinalProjectWebsite/scripts/produceAPI.js
+++ FinalProject/FinalProjectWebsite/scripts/produceAPI.js
@@ -6,6 +6,7 @@ const ingredient3 = document.querySelector('#ingredient3');
 const submitted = document.querySelector('#submitButton');
 const orderFormHeader = document.querySelector('.orderNow');
 const orderForm = document.querySelector('.form');
+const newDiv = document.querySelector('.reciept');
 
 
 async function getProduce(){
@@ -34,56 +35,49 @@ function showProduce(produce){
 getProduce();
 
 //Put up a reciept when submitting a recepie:
-
-submitted.addEventListener('click', () => {
-    orderFormHeader.style.display = "none";
-    orderForm.style.display = 'none';
-
-    const firstName = document.querySelector('#firstName').value;
-    const email = document.querySelector('#email').value;
-    const phone = document.querySelector('#phone').value;
-    const ingredient1 = document.querySelector('#ingredient1').value;
-    const ingredient2 = document.querySelector('#ingredient2').value;
-    const ingredient3 = document.querySelector('#ingredient3').value;
-    const specialInstructions = document.querySelector('#specialInstructions').value;
+submitted.addEventListener('click', () =>{
+        const firstName = document.querySelector('#firstName').value;
+        const email = document.querySelector('#email').value;
+        const phone = document.querySelector('#phone').value;
+        const ingredient1 = document.querySelector('#ingredient1').value;
+        const ingredient2 = document.querySelector('#ingredient2').value;
+        const ingredient3 = document.querySelector('#ingredient3').value;
+        const specialInstructions = document.querySelector('#specialInstructions').value;
 
 
+        let header = document.createElement('h2');
+        let checkFirstName = document.createElement('p');
+        let checkName = document.createElement('p');
+        let checkEmail = document.createElement('p');
+        let checkPhone = document.createElement('p');
+        let checkIngredient1 = document.createElement('p');
+        let checkIngredient2 = document.createElement('p');
+        let checkIngredient3 = document.createElement('p');
+        let checkInstructions = document.createElement('p');
+        let correctButton = document.createElement('button');
 
-    let divider = document.createElement('div');
-    let header = document.createElement('h2');
-    let checkName = document.createElement('p');
-    let checkEmail = document.createElement('p');
-    let checkPhone = document.createElement('p');
-    let checkIngredient1 = document.createElement('p');
-    let checkIngredient2 = document.createElement('p');
-    let checkIngredient3 = document.createElement('p');
-    let checkInstructions = document.createElement('p');
-    let correctButton = document.createElement('input');
-    let firstNameValue = firstName.value;
+        header.textContent = 'We Recieved Your Order!'
+        checkName.textContent = `Name: ${firstName}`;
+        checkPhone.textContent = `Phone: ${phone}`;
+        checkEmail.textContent = `Email: ${email}`;
+        checkIngredient1.textContent = `First Ingredient: ${ingredient1}`;
+        checkIngredient2.textContent = `Second Ingredient ${ingredient1}`;
+        checkIngredient3.textContent = `Third Ingredient ${ingredient1}`;
+        checkInstructions.textContent = `Special Instructions ${specialInstructions}`;
+        correctButton.textContent = "Go Back";
 
-    divider.className = 'orderReciept';
-    header.textContent = 'We Recieved Your Order!'
-    checkName.textContent = `Name: ${firstName}`;
-    checkPhone.textContent = `Phone: ${phone}`;
-    checkEmail.textContent = `Email: ${email}`;
-    checkIngredient1.textContent = `First Ingredient: ${ingredient1}`;
-    checkIngredient2.textContent = `Second Ingredient ${ingredient1}`;
-    checkIngredient3.textContent = `Third Ingredient ${ingredient1}`;
-    checkInstructions.textContent = `Fourth Ingredient ${specialInstructions}`;
-    correctButton.type = 'submit';
-    correctButton.value = 'Back';
+        newDiv.appendChild(header);
+        newDiv.appendChild(checkName);
+        newDiv.appendChild(checkEmail);
+        newDiv.appendChild(checkPhone);
+        newDiv.appendChild(checkIngredient1);
+        newDiv.appendChild(checkIngredient2);
+        newDiv.appendChild(checkIngredient3);
+        newDiv.appendChild(checkInstructions);
+        newDiv.appendChild(correctButton);
 
-    divider.appendChild(header);
-    divider.appendChild(checkName);
-    divider.appendChild(checkEmail);
-    divider.appendChild(checkPhone);
-    divider.appendChild(checkIngredient1);
-    divider.appendChild(checkIngredient2);
-    divider.appendChild(checkIngredient3);
-    divider.appendChild(checkInstructions);
-    divider.appendChild(correctButton);
-
-    mainInfo.appendChild(divider);
-
+    correctButton.addEventListener('click', () => {
+        window.location.reload();
+        });
 });
 
